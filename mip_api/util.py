c922a696cfa3bd34ca95a22beeb19c0536104d32
Joni Harker <joni.harker@sagebase.org>
2025-07-18 12:27:13 -0700

    [IT-4485] Add endpoint to show GL account balances
    Add a new API endpoint `/balances` to output a CSV of GL account
    balances appropriate for sending to FloQast.
    
    During the first week of the month, process the balance for the previous
    month, otherwise process the month-to-date balance.
    
    The pre-existing endpoints used the Program segment of the chart of
    accounts, but the new endpoint uses the GL segment of the chart of
    accounts. Refactor fetching the chart of accounts to support arbitrary
    segments.

--- mip_api/util.py
+++ mip_api/util.py
@@ -1,4 +1,24 @@
+import json
+import logging
 import os
+from datetime import date, timedelta
+
+LOG = logging.getLogger(__name__)
+LOG.setLevel(logging.DEBUG)
+
+
+def build_return_json(code, body):
+    return {
+        "statusCode": code,
+        "body": json.dumps(body, indent=2),
+    }
+
+
+def build_return_text(code, body):
+    return {
+        "statusCode": code,
+        "body": body,
+    }
 
 
 def get_os_var(varnam):
@@ -15,6 +35,12 @@ def parse_codes(codes):
     return data
 
 
+def _param_str(params, param):
+    if params and param in params:
+        return params[param]
+    return ""
+
+
 def _param_bool(params, param):
     if params and param in params:
         if params[param].lower() not in ["false", "no", "off"]:
@@ -22,19 +48,27 @@ def _param_bool(params, param):
     return False
 
 
-def param_inactive_bool(params):
+def _param_hide_inactive_bool(params):
+    # Default is to hide inactive codes
     return not _param_bool(params, "show_inactive_codes")
 
 
-def param_other_bool(params):
+def _param_show_other_bool(params):
+    # Default is to hide "other" code
     return _param_bool(params, "show_other_code")
 
 
-def param_no_program_bool(params):
+def _param_show_no_program_bool(params):
+    # Default is to show "no program" code
     return not _param_bool(params, "hide_no_program_code")
 
 
-def param_limit_int(params):
+def _param_date_str(params):
+    # Default is to show "no program" code
+    return _param_str(params, "target_date")
+
+
+def _param_limit_int(params):
     if params and "limit" in params:
         try:
             return int(params["limit"])
@@ -44,8 +78,51 @@ def param_limit_int(params):
     return 0
 
 
-def param_priority_list(params):
+def _param_priority_list(params):
     if params and "priority_codes" in params:
         return parse_codes(params["priority_codes"])
 
     return None
+
+
+def params_dict(event):
+    _params = {}
+    if "queryStringParameters" in event:
+        _params = event["queryStringParameters"]
+        LOG.debug(f"Query-string _parameters: {_params}")
+    params = {
+        "hide_inactive": _param_hide_inactive_bool(_params),
+        "limit": _param_limit_int(_params),
+        "priority_codes": _param_priority_list(_params),
+        "show_no_program": _param_show_no_program_bool(_params),
+        "show_other": _param_show_other_bool(_params),
+        "date": _param_date_str(_params),
+    }
+    return params
+
+
+def target_period(when=None):
+    if when is None:
+        target_day = date.today()
+    else:
+        target_day = date.fromisoformat(when)
+    LOG.info(f"Processing period for {target_day}")
+
+    if target_day.day <= 7:
+        # at the beginning of the month, look at previous month
+        _first = target_day.replace(day=1)  # first day of target month
+        end = _first - timedelta(days=1)  # last day of previous month
+        start = end.replace(day=1)  # first day of previous month
+
+        end_str = end.isoformat()
+        start_str = start.isoformat()
+    else:
+        # otherwise look at month-to-date
+        start = target_day.replace(day=1)
+        end_str = target_day.isoformat()
+        start_str = start.isoformat()
+
+    LOG.info(f"Start day is {start_str}")
+    LOG.info(f"End day is {end_str}")
+
+    return start_str, end_str
